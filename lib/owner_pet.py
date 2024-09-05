e27ad27c7fa427cf398540d88ba27bd30e8583f2
(dennoAiden) <(kipkiruidennis25@gmail.com)>
2024-09-05 14:10:03 +0300

    0ne to many

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -4,18 +4,34 @@ class Pet:
 
     def __init__(self, name, pet_type, owner=None):
         self.name = name
-        if pet_type not in Pet.PET_TYPES:
-            raise Exception(f"{pet_type} is not a valid pet type. Choose from {', '.join(Pet.PET_TYPES)}.")
-        self.pet_type = pet_type
-        self.owner = None
-
-        if owner:
-            if isinstance(owner, Owner):
-                self.owner = owner
-            else:
-                raise Exception("Owner must be an instance of Owner class.")
-        
+        self.pet_type = pet_type  # This will use the pet_type property setter
+        self.owner = owner  # This will use the owner property setter
         Pet.all.append(self)
+
+    # Getter for pet_type
+    @property
+    def pet_type(self):
+        return self._pet_type
+
+    # Setter for pet_type
+    @pet_type.setter
+    def pet_type(self, value):
+        if value not in Pet.PET_TYPES:
+            raise Exception(f"{value} is not a valid pet type. Choose from {', '.join(Pet.PET_TYPES)}.")
+        self._pet_type = value
+
+    # Getter for owner
+    @property
+    def owner(self):
+        return self._owner
+
+    # Setter for owner
+    @owner.setter
+    def owner(self, value):
+        if value is not None and not isinstance(value, Owner):
+            raise Exception("Owner must be an instance of Owner class.")
+        self._owner = value
+
 
 
 class Owner:
